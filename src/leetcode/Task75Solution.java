a0bf1a0180eb675955a19a90837b3ed9227bbf8e
Georgii Naumov <gmnaumov@yandex.ru>
2022-12-06 21:47:39 +0300

    Task 75 Details

--- src/leetcode/Task75Solution.java
+++ src/leetcode/Task75Solution.java
@@ -1,5 +1,19 @@
 package leetcode;
 
+/***
+ * https://leetcode.com/problems/sort-colors/
+ * 75. Sort Colors
+ *
+ * Given an array nums with n objects colored red, white, or blue,
+ * sort them in-place so that objects of the same color are adjacent,
+ * with the colors in the order red, white, and blue.
+ *
+ * We will use the integers 0, 1, and 2 to represent the color
+ * red, white, and blue, respectively.
+ *
+ * You must solve this problem without using the library's sort function.
+ *
+ */
 public class Task75Solution {
     public static void main(String[] args) {
         int[] a = new int[]{2, 0, 2, 1, 1, 0};
@@ -24,5 +38,38 @@ public class Task75Solution {
         for (i = cnt[0] + cnt[1]; i < cnt[0] + cnt[1] + cnt[2]; i++) {
             nums[i] = 2;
         }
+    }
+
+    /***
+     * We traverse the array and use two variables:
+     *
+     * 1) zeroIndex to indicate the index where we should put 0
+     * 2) twoIndex to indicate the index where we should put 2
+     * If current element a[i] is 0, we swap a[i] and a[zeroIndex],
+     * if a[i] is 2, we swap a[i] and a[twoIndex],
+     * if a[i] is one, we do nothing and continue to process next element.
+     *
+     * @param nums
+     */
+    public static void sortColors2(int[] nums) {
+        int zIndex = 0;
+        int tIndex = nums.length - 1;
+        int i = 0;
+
+        while (i <= tIndex) {
+            if (nums[i] == 0) {
+                swap(nums, zIndex++, i++);
+            } else if (nums[i] == 2) {
+                swap(nums, tIndex--, i);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
